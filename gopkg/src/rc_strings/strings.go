27a8b1b764260901d20d84117deaf1b2341e3ea9
rcpenny <rongchen0905@gmail.com>
2022-04-16 18:55:34 +0800

    add Join() rc_strings

---
+++ gopkg/src/rc_strings/strings.go
@@ -0,0 +1,25 @@
+package rc_strings
+
+// Join concatenates the elements of its first argument to create a single string. The separator
+// string sep is placed between elements in the resulting string.
+func Join(elems []string, sep string) string {
+	switch len(elems) {
+	case 0:
+		return ""
+	case 1:
+		return elems[0]
+	}
+	n := len(sep) * (len(elems) - 1)
+	for i := 0; i < len(elems); i++ {
+		n += len(elems[i])
+	}
+
+	var b Builder
+	b.Grow(n)
+	b.WriteString(elems[0])
+	for _, s := range elems[1:] {
+		b.WriteString(sep)
+		b.WriteString(s)
+	}
+	return b.String()
+}
